f1e3b372a2d554d0e1e8439ed8bbd3cbb07d51ff
Jin Zhou <ferryzhou@gmail.com>
2014-01-06 16:35:30 -0500

    u

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ var panel_contents = {
   "Data Integration / ETL": {
     "Free GUI Tools": ["Kettle", "Talend", "CloverETL CE"],
 	"Commercial GUI Tools": ["Microsoft", "Infomatica", "IBM", "SAP", "CloverETL", "SAS Data Management"],
-	"Programming/API": ["SAS Scripts", "JAVA/JDBC", "Python"]
+	"Programming/API": ["SAS Scripts", "JAVA/JDBC", "Python", "Spring Data"]
   },
   "Good Data": {
     "Text Files":   ["CSV", "XML", "JSON"],
@@ -18,7 +18,7 @@ var panel_contents = {
   },
   "Data Presentation / BI": {
     "Free GUI Tools": ["Tableau Public", "Pentaho BI CE", "QlikView"],
-	"Commercial GUI Tools": ["Microstrategy", "Tableau", "Pentaho EE"],
+	"Commercial GUI Tools": ["Microstrategy", "Tableau", "Pentaho EE", "Tibco"],
 	"Programming/API": ["SAS", "R", "HTML/JS", "Web API", "JAVA"]    
   }
 }
